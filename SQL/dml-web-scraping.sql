09c4cad1b69afb57587b4e246015a442092a05b7
AlexxFuentes <aafuentes@unah.hn>
2023-08-17 20:19:06 -0600

    Consultas para responder preguntas

--- SQL/dml-web-scraping.sql
+++ SQL/dml-web-scraping.sql
@@ -7,31 +7,32 @@ USE IIDDBDWorldFactbook;
 CREATE TABLE Countries (
     id SERIAL PRIMARY KEY,
     tex_name VARCHAR(60) NOT NULL UNIQUE COMMENT "Nombre de país",
-    dec_population DECIMAL(20,5) NOT NULL COMMENT "Cantidad de habitantes",
+    dec_population DECIMAL(20,2) NOT NULL COMMENT "Cantidad de habitantes",
     dec_population_growth_percent DECIMAL(30,5) NOT NULL COMMENT "Porcentaje de crecimiento poblacional",
-    dec_area_km_sq DECIMAL(20,5) NOT NULL COMMENT "Área en km cuadrados",
+    dec_area_km_sq DECIMAL(20,2) NOT NULL COMMENT "Área en km cuadrados",
     tex_map_references VARCHAR(100) NOT NULL COMMENT "Referencias del país",
     int_coastline_km_sq INT NOT NULL COMMENT "ÁREA DE LÍNEA COSTERA",
     int_max_elevation_km INT NOT NULL COMMENT "Elevación máxima",
     int_min_elevation_km INT NOT NULL COMMENT "Punto más bajo del país",
-    dec_population_density DECIMAL(20,5) NOT NULL COMMENT "Densidad poblacional"
+    dec_population_density DECIMAL(20,2) NOT NULL COMMENT "Densidad poblacional"
 ) COMMENT "Datos generales del país";
 
 CREATE TABLE Economy (
     id SERIAL PRIMARY KEY,
     id_countries_fk BIGINT UNSIGNED NOT NULL COMMENT "Referencia hacia countries",
-    dec_GDP_general_billion_of_dollars DECIMAL(30,5) NOT NULL COMMENT "PIB PAÍS",
-    dec_GDP_contribution_agriculture_percent DECIMAL(20,5) NOT NULL COMMENT "Aporte de agricultura al PIB",
-    dec_GDP_contribution_industry_percent DECIMAL(20,5) NOT NULL COMMENT "Aporte de industria al PIB",
-    dec_GDP_contribution_service_percent DECIMAL(20,5) NOT NULL COMMENT "Aporte de servicios al PIB",
+    dec_GDP_general_billion_of_dollars DECIMAL(30,2) NOT NULL COMMENT "PIB PAÍS",
+    dec_GDP_contribution_agriculture_percent DECIMAL(20,2) NOT NULL COMMENT "Aporte de agricultura al PIB",
+    dec_GDP_contribution_industry_percent DECIMAL(20,2) NOT NULL COMMENT "Aporte de industria al PIB",
+    dec_GDP_contribution_service_percent DECIMAL(20,2) NOT NULL COMMENT "Aporte de servicios al PIB",
     FOREIGN KEY (id_countries_fk) REFERENCES Countries(id) ON DELETE CASCADE
 ) COMMENT "Datos de la economía del país";
 
 CREATE TABLE Education (
     id SERIAL PRIMARY KEY,
     id_countries_fk BIGINT UNSIGNED NOT NULL COMMENT "Referencia hacia countries",
+    dec_literacy_percent DECIMAL(10,2) NOT NULL COMMENT "Porcentaje de personas alfabetizadas",
     int_school_life_time INT NOT NULL COMMENT "Tiempo en que la persona está en la escuela",
-    dec_education_cost_percent_GDP DECIMAL NOT NULL COMMENT "Costo en PIB de la educación",
+    dec_education_cost_percent_GDP DECIMAL(10,2) NOT NULL COMMENT "Costo en PIB de la educación",
     FOREIGN KEY (id_countries_fk) REFERENCES Countries(id) ON DELETE CASCADE
 ) COMMENT "Datos de la educación del país";
 
